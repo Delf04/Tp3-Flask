92416fe5646f270fbf5a2b45e26e519d2c12f4fa
simoncanorio <sicanoriohellwing@mail.austral.edu.ar>
2025-05-23 09:07:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,86 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
-
+import csv
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Persona(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc) )
-
-    def __repr__(self):
-        return 'Task %r' % self.id
+    name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(20))
+    country = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    education_level = db.Column(db.String(50))
+    employment_status = db.Column(db.String(50))
+    annual_income_usd = db.Column(db.Float)
+    marital_status = db.Column(db.String(50))
+    children_count = db.Column(db.Integer)
+    smokes_per_day = db.Column(db.Integer)
+    drinks_per_week = db.Column(db.Integer)
+    age_started_smoking = db.Column(db.Integer)
+    age_started_drinking = db.Column(db.Integer)
+    attempts_to_quit_smoking = db.Column(db.Integer)
+    attempts_to_quit_drinking = db.Column(db.Integer)
+    has_health_issues = db.Column(db.Boolean)
+    mental_health_status = db.Column(db.String(50))
+    exercise_frequency = db.Column(db.String(50))
+    diet_quality = db.Column(db.String(50))
+    sleep_hours = db.Column(db.Float)
+    bmi = db.Column(db.Float)
+    social_support = db.Column(db.String(50))
+    therapy_history = db.Column(db.String(50))
 
 with app.app_context():
-    db.create_all()  # Esto creará la tabla 'todo'
+    db.create_all()
     print("¡Base de datos y tablas creadas correctamente!")
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content'] 
-        new_task = Todo(content=task_content)
+    personas = Persona.query.all()
+    return render_template('index.html', personas=personas)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except: 
-            return "There was an error adding your taks"
-
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
+@app.route('/cargar', methods=['POST'])
+def cargar_csv():
+    file = request.files['file']
+    if file and file.filename.endswith('.csv'):
+        stream = file.stream.read().decode('utf-8').splitlines()
+        reader = csv.DictReader(stream)
+        for row in reader:
+            persona = Persona(
+                id=int(row['id']),
+                name=row['name'],
+                age=int(row['age']),
+                gender=row['gender'],
+                country=row['country'],
+                city=row['city'],
+                education_level=row['education_level'],
+                employment_status=row['employment_status'],
+                annual_income_usd=float(row['annual_income_usd']),
+                marital_status=row['marital_status'],
+                children_count=int(row['children_count']),
+                smokes_per_day=int(row['smokes_per_day']),
+                drinks_per_week=int(row['drinks_per_week']),
+                age_started_smoking=int(row['age_started_smoking']),
+                age_started_drinking=int(row['age_started_drinking']),
+                attempts_to_quit_smoking=int(row['attempts_to_quit_smoking']),
+                attempts_to_quit_drinking=int(row['attempts_to_quit_drinking']),
+                has_health_issues=row['has_health_issues'].strip().lower() == 'true',
+                mental_health_status=row['mental_health_status'],
+                exercise_frequency=row['exercise_frequency'],
+                diet_quality=row['diet_quality'],
+                sleep_hours=float(row['sleep_hours']),
+                bmi=float(row['bmi']),
+                social_support=row['social_support'],
+                therapy_history=row['therapy_history']
+            )
+            db.session.add(persona)
         db.session.commit()
-        return redirect('/')
-    except:
-        return "There was an error deleting your task"
-    
-
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    task_to_update = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task_to_update.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an error updating your taks"
-    else:
-        return render_template('update.html', task=task_to_update)
-
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
